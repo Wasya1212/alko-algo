5297807f11d4061bfa336949164c891ccf40315e
Vasyl_Ukrainets <vu@msm-tech.net>
2021-07-15 16:17:09 +0300

    renamed func

--- 1.js
+++ 1.js
@@ -9,8 +9,8 @@
 */
 
 // MAIN FUNCTION
-const lastDigitOfNaturalNumber = n => (n <= 100 && n >= 1) ? n % 10 : null;
+const getLastDigit = n => (n <= 100 && n >= 1) ? n % 10 : null;
 
 // RESULTS
 const testValues = [-230, -100, -99, 0, 1, 37, 65, 99, 100, 128, '-2', '0', '15', '485', '12w'];
-testValues.forEach(n => console.log(`n = ${n} -> ${lastDigitOfNaturalNumber(n)}`));
+testValues.forEach(n => console.log(`n = ${n} -> ${getLastDigit(n)}`));
